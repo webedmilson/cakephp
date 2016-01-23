5b31711215c520a29b053a50cee2345e6fa69982
OpenShift System User <webedmilson@hotmail.com>
2016-01-23 08:37:36 -0200

    teste

--- app/index.php
+++ app/index.php
@@ -13,5 +13,5 @@
  * @since         CakePHP(tm) v 0.10.0.1076
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
+die();
 require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
